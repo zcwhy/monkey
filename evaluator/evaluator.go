08882f84848e6a9039c206fe0656e4b6dfb19310
zcwhy <98001448+zcwhy@users.noreply.github.com>
2024-05-31 21:31:49 +0800

    function, closure

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -56,7 +56,7 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		if isError(val) {
 			return val
 		}
-		return &object.ReturnValue{Value: val}
+		env.Set(node.Name.Value, val)
 
 	case *ast.ReturnStatement:
 		val := Eval(node.ReturnValue, env)
@@ -71,6 +71,21 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return newError("identifier not found: " + node.Value)
 		}
 		return val
+
+	case *ast.FunctionLiteral:
+		return &object.Function{Parameters: node.Parameters, Body: node.Body, Env: env}
+
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+		return applyFunction(function, args)
 	}
 	return nil
 }
@@ -222,6 +237,37 @@ func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 
 	value := right.(*object.Integer).Value
 	return &object.Integer{Value: -value}
+}
+
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+	return result
+}
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	env := object.NewEnclosedEnvironment(function.Env)
+	for paramIdx, param := range function.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+
+	evaluated := Eval(function.Body, env)
+	if returnValue, ok := evaluated.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+	return evaluated
 }
 
 func newError(format string, a ...interface{}) *object.Error {
